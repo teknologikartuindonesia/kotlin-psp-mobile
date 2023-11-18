3de313eaf480ea260d41a0d0ffc89cac1413166d
ibnu naufal <ibnunaufal5758@gmail.com>
2023-11-18 15:47:58 +0700

    configuring home page content

--- app/src/main/java/id/co/pspmobile/ui/HomeBottomNavigation/message/MessageFragment.kt
+++ app/src/main/java/id/co/pspmobile/ui/HomeBottomNavigation/message/MessageFragment.kt
@@ -1,4 +1,4 @@
-package id.co.pspmobile.ui.HomeBottomNavigation.notifications
+package id.co.pspmobile.ui.HomeBottomNavigation.message
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import id.co.pspmobile.databinding.FragmentNotificationsBinding
+import id.co.pspmobile.databinding.FragmentMessageBinding
 
-class NotificationsFragment : Fragment() {
+class MessageFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentMessageBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(MessageViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentMessageBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
