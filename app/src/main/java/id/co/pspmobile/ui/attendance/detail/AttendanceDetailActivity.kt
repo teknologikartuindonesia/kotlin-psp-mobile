0ad9d69a2fc5724858f2465de3eda5b16eb34a4e
Yudi Kurniawan <yd.kurniawan@ymail.com>
2023-11-22 11:31:23 +0700

    update attendance

---
+++ app/src/main/java/id/co/pspmobile/ui/attendance/detail/AttendanceDetailActivity.kt
@@ -0,0 +1,12 @@
+package id.co.pspmobile.ui.attendance.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.pspmobile.R
+
+class AttendanceDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_attendance_detail)
+    }
+}
