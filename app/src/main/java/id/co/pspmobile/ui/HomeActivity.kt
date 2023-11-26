2ddd420555ec95347944633d72335c8e657192d6
ibnu naufal <ibnunaufal5758@gmail.com>
2023-11-26 10:16:20 +0700

    configure back button press

--- app/src/main/java/id/co/pspmobile/ui/HomeActivity.kt
+++ app/src/main/java/id/co/pspmobile/ui/HomeActivity.kt
@@ -1,20 +1,24 @@
 package id.co.pspmobile.ui
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import dagger.hilt.android.AndroidEntryPoint
 import id.co.pspmobile.R
 import id.co.pspmobile.databinding.ActivityHomeBinding
+import id.co.pspmobile.ui.Utils.showToast
 
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private var exit = false
+    private lateinit var navController: NavController
+    private lateinit var menuBottom : ChipNavigationBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +27,11 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        navController = findNavController(R.id.nav_host_fragment_activity_home)
 
-        var menu_bottom = binding.bottomNavBar
-        menu_bottom.setItemSelected(R.id.navigation_home, true)
-        menu_bottom.setOnItemSelectedListener {
+        menuBottom = binding.bottomNavBar
+        menuBottom.setItemSelected(R.id.navigation_home, true)
+        menuBottom.setOnItemSelectedListener {
             when(it){
                 R.id.navigation_home -> {
                     navController.navigate(R.id.navigation_home)
@@ -44,5 +48,21 @@ class HomeActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onBackPressed() {
+        if (navController.currentDestination?.id == R.id.navigation_home) {
+            if (exit) {
+                finish()
+            } else {
+                showToast(this, resources.getString(R.string.press_back_to_exit), "short")
+                exit = true
+                Handler().postDelayed({ exit = false }, 3 * 1000)
+            }
+        } else {
+            navController.navigate(R.id.navigation_home)
+            menuBottom = binding.bottomNavBar
+            menuBottom.setItemSelected(R.id.navigation_home, true)
+        }
     }
 }
