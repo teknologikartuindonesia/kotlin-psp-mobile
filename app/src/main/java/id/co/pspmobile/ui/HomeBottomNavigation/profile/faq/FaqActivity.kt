965f18a7640efae11ba7b37dd510e77b26eb430b
ibnu naufal <ibnunaufal5758@gmail.com>
2023-11-28 14:09:08 +0700

    improve

--- app/src/main/java/id/co/pspmobile/ui/HomeBottomNavigation/profile/faq/FaqActivity.kt
+++ app/src/main/java/id/co/pspmobile/ui/HomeBottomNavigation/profile/faq/FaqActivity.kt
@@ -5,20 +5,29 @@ import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
 import id.co.pspmobile.R
 import id.co.pspmobile.databinding.ActivityFaqBinding
+import id.co.pspmobile.ui.dialog.DialogCS
 
 @AndroidEntryPoint
 class FaqActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFaqBinding
     private var faqList = ArrayList<FaqModel>()
+    private var isFromFaq = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFaqBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        isFromFaq = intent.getBooleanExtra("isFromFaq", true)
+
         binding.btnBack.setOnClickListener {
             finish()
+        }
+
+        binding.btnCs.setOnClickListener {
+            val dialog = DialogCS()
+            dialog.show(supportFragmentManager, dialog.tag)
         }
 
         showFaq()
@@ -27,10 +36,10 @@ class FaqActivity : AppCompatActivity() {
 
     fun showFaq(){
         faqList.add(FaqModel(resources.getString(R.string.faq_q_forgot_username), resources.getString(R.string.faq_a_forgot_username), false))
-        faqList.add(FaqModel(resources.getString(R.string.faq_q_forgot_password), resources.getString(R.string.faq_a_forgot_password), false))
-        faqList.add(FaqModel(resources.getString(R.string.faq_q_topup), resources.getString(R.string.faq_a_topup), false))
-        faqList.add(FaqModel(resources.getString(R.string.faq_q_pay_invoice), resources.getString(R.string.faq_a_pay_invoice), false))
-        faqList.add(FaqModel(resources.getString(R.string.faq_q_invoice_not_shown), resources.getString(R.string.faq_a_invoice_not_shown), false))
+        faqList.add(FaqModel(resources.getString(R.string.faq_q_forgot_password), resources.getString(R.string.faq_a_forgot_password), true))
+        faqList.add(FaqModel(resources.getString(R.string.faq_q_topup), resources.getString(R.string.faq_a_topup), true))
+        faqList.add(FaqModel(resources.getString(R.string.faq_q_pay_invoice), resources.getString(R.string.faq_a_pay_invoice), true))
+        faqList.add(FaqModel(resources.getString(R.string.faq_q_invoice_not_shown), resources.getString(R.string.faq_a_invoice_not_shown), true))
 
         val faqAdapter = FaqAdapter()
         faqAdapter.setFaqList(faqList)
